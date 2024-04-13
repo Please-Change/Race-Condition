290665817aa3fe25ffc1a47d8763c2f493b63bbb
Wilf Silver <git@wilfsilver.co.uk>
2024-04-13 14:25:57 +0100

    Add monaco editor

--- src/App.svelte
+++ src/App.svelte
@@ -2,9 +2,21 @@
   import svelteLogo from "./assets/svelte.svg";
   import viteLogo from "/vite.svg";
   import Counter from "./components/Counter.svelte";
+  import Editor from "./components/Editor.svelte";
+  import type { EditorImpl } from "@lib/editor";
+
+  let editor: EditorImpl;
 </script>
 
 <main>
   <h2 class="font-brand text-2xl font-bold">Race Condition</h2>
-  
+  <Editor
+    language="typescript"
+    initDocument={[
+      "function hello() {",
+      "  console.log('hello world');",
+      "}",
+    ].join("\n")}
+    bind:editor
+  />
 </main>
