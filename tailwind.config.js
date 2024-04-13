01f6350e582a50b99047b02f9e51bb1bd5f0eb20
Sammy99jsp <sammy99jsp@sammy99jsp.dev>
2024-04-13 13:13:37 +0100

    Add fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        brand: ["Fixed Sys", "monospace"],
+        sans: ["JetBrains Mono", "monospace"],
+      },
+      fontSize: {
+        "3xl": "4rem",
+        "2xl": "3rem",
+        "xl": "2rem",
+      }
+    },
   },
   plugins: [],
 };
